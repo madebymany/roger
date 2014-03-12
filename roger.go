69181a4659fe1e300005ae1b0bda9dc025692996
Dan Brown <dan@madebymany.co.uk>
2014-03-12 11:33:14 +0000

    Now allows ranges in time specs

--- roger.go
+++ roger.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -20,7 +21,7 @@ type timeSpec struct {
 	instances []int
 }
 
-var explicitTimeSpecPattern = regexp.MustCompile(`\A(([\d,])+|\*)(/(\d+))?\z`)
+var timeSpecPattern = regexp.MustCompile(`\A(?P<instances>((\d+(-\d+)?,)*(\d+(-\d+)?))+|\*)(/(?P<every>\d+))?\z`)
 
 func main() {
 	log.SetFlags(0)
@@ -32,6 +33,9 @@ func main() {
 
 	minSpec := parseTimeSpec(*minSpecStr)
 	hourSpec := parseTimeSpec(*hourSpecStr)
+
+	fmt.Printf("minSpec: %#v\n", minSpec)
+	fmt.Printf("hourSpec: %#v\n", hourSpec)
 
 	var cmd *exec.Cmd
 	if *inShell {
@@ -59,32 +63,46 @@ func main() {
 
 func parseTimeSpec(in string) (out timeSpec) {
 	out.every = 1
-	var err error
 
-	if in == "*" {
-		return
-	} else if m := explicitTimeSpecPattern.FindStringSubmatch(in); m != nil {
-		if m[1] != "*" {
-			instancesStrs := strings.Split(m[1], ",")
-			out.instances = make([]int, len(instancesStrs))
-			for i, str := range instancesStrs {
-				out.instances[i], err = strconv.Atoi(str)
-				if err != nil {
-					panic(err)
-				}
-			}
-		}
-
-		if m[4] != "" {
-			out.every, err = strconv.Atoi(m[4])
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		return
-	} else {
+	match := timeSpecPattern.FindStringSubmatch(in)
+	if match == nil {
 		log.Fatalf("Invalid spec: '%s'\n", in)
+		return
+	}
+
+	var everyIdx, instancesIdx int
+	for i, n := range timeSpecPattern.SubexpNames() {
+		switch n {
+		case "every":
+			everyIdx = i
+		case "instances":
+			instancesIdx = i
+		}
+	}
+
+	if match[everyIdx] != "" {
+		out.every = mustAtoi(match[everyIdx])
+	}
+
+	if match[instancesIdx] != "*" {
+		instancesStrs := strings.Split(match[instancesIdx], ",")
+		out.instances = make([]int, 0, len(instancesStrs))
+
+		for _, str := range instancesStrs {
+			rangeSplit := strings.Split(str, "-")
+			switch len(rangeSplit) {
+			case 1:
+				out.instances = append(out.instances,
+				mustAtoi(rangeSplit[0]))
+			case 2:
+				from, to := mustAtoi(rangeSplit[0]), mustAtoi(rangeSplit[1])
+				for i := from; i <= to; i++ {
+					out.instances = append(out.instances, i)
+				}
+			default:
+				panic("invalid range")
+			}
+		}
 	}
 	return
 }
@@ -103,4 +121,12 @@ func (self timeSpec) matches(moment int) bool {
 		}
 	}
 	return false
+}
+
+func mustAtoi(s string) (n int) {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
